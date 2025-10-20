d4035e38b660e39f95cb336ce9706f8b331c9f00
akxm110 <impeace53@gmail.com>
2025-10-21 08:55:42 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+// 서비스 워커 버전
+const CACHE_NAME = 'thumbnail-memo-v1';
+const urlsToCache = [
+  '/index.html',
+  '/share-receiver.html'
+];
+
+// 설치 이벤트
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// 활성화 이벤트
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch 이벤트
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에 있으면 반환, 없으면 네트워크 요청
+        return response || fetch(event.request);
+      })
+  );
+});
+
